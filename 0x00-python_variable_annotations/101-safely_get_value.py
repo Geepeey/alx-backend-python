8652d39fac51c642d027ebc3c0f32480983cfc3c
Godspower Udokpoh <harshmallins@example.com>
2023-07-06 13:37:25 +0000

    ...

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -14,7 +14,6 @@ from typing import Mapping, Any, TypeVar, Union
 
 T = TypeVar('T')
 
-
 def safely_get_value(dct: Mapping, key: Any, default: T = None
                      ) -> Union[Any, T]:
     """Return value and annotation type"""
