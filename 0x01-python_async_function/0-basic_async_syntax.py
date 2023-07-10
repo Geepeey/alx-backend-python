5d4a47aecdaa3b654edcf55aaecf76e8214f870c
Godspower Udokpoh <harshmallins@example.com>
2023-07-10 13:18:17 +0000

    Syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
-    """Returns delay"""
+async def wait_random(max_delay: int = 10) -> float:
+    """async coroutine"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
