b6f93e1dccb80a8ba83c7bba5ee0bb120dc3b7ec
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-07-11 01:02:36 +0100

    trying importing

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,20 +7,7 @@ to wait_n except task_wait_random is being called
 import asyncio
 from typing import List
 from asyncio.tasks import Task
-
-
-async def wait_random(max_delay: int) -> float:
-    """Function def wait_random"""
-    delay = random() * max_delay
-    await asyncio.sleep(delay)
-    return delay
-
-
-def task_wait_random(max_delay: int) -> Task:
-    """Function def task_wait_random"""
-    coro = wait_random(max_delay)
-    task = asyncio.create_task(coro)
-    return task
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
